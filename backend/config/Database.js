dac445f94ad2782b1f9e1391e1a8373de1ebcbed
mraq123 <hairaffi2@gmail.com>
2024-06-09 12:19:52 +0700

    deploy baru nih bismillah

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("smart_audio_db", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const db = new Sequelize(
+  "bxeulshj14cdw15sx6el",
+  "uaomd9vmldrjsewh",
+  "j7O0Wo3RZYUWx5MieZX2",
+  {
+    host: "bxeulshj14cdw15sx6el-mysql.services.clever-cloud.com",
+    dialect: "mysql",
+  }
+);
 
 export default db;
