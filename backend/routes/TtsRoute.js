007bdd6a7a18fef48b3a2e2661c06036d3b37d88
mraq123 <hairaffi2@gmail.com>
2024-06-09 12:33:21 +0700

    sjkjhdskjhjks

--- backend/routes/TtsRoute.js
+++ backend/routes/TtsRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { verifyUser } from "../middleware/AuthUser.js"; // Jika Anda memiliki middleware ini
-import { getTts } from "../controllers/Tts.js ";
+import { getTts } from "../controllers/Tts.js";
 
 const router = express.Router();
 
